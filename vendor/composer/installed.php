2064792c3fe2fb49fcf2db7bf90df9258ac27f8c
Nipun4338 <49658560+Nipun4338@users.noreply.github.com>
2021-04-30 22:12:55 +0600

    composer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '6.4.1',
+    'version' => '6.4.1.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'tecnickcom/tcpdf',
+  ),
+  'versions' => 
+  array (
+    'tecnickcom/tcpdf' => 
+    array (
+      'pretty_version' => '6.4.1',
+      'version' => '6.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
