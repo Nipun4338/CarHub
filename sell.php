7fd799f56b409183f9b2dd9953be81c00d38d518
Nipun4338 <49658560+Nipun4338@users.noreply.github.com>
2021-04-30 22:22:34 +0600

    meta mistake solved

--- sell.php
+++ sell.php
@@ -6,7 +6,7 @@ include('database/dbconfig.php'); ?>
 	<title>Subscribe | CarHub</title>
 	<meta charset="UTF-8">
 
-	<meta name="viewport" content="width-device-width, initial scale = 1.0">
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0">
 
 	<script src = "https://code.jquery.com/jquery-2.1.3.min.js"></script>
 
--- team.php
+++ team.php
@@ -25,7 +25,7 @@ if($noOfRows){
 	<title>Team | CarHub</title>
 	<meta charset="UTF-8">
 
-	<meta name="viewport" content="width-device-width, initial scale = 1.0">
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0">
 
 	<script src = "https://code.jquery.com/jquery-2.1.3.min.js"></script>
 
